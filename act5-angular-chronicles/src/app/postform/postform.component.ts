36a7dcbb2fd77b44165229c5535410a7d38747e1
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-02-17 20:10:14 +0100

    Modified postform: added delete and image capabilities

--- act5-angular-chronicles/src/app/postform/postform.component.ts
+++ act5-angular-chronicles/src/app/postform/postform.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-postform',
@@ -8,8 +8,19 @@ import { Component } from '@angular/core';
 })
 export class PostformComponent {
   date = new Date();
+  imageToggle = signal(true);
+  deleteModalToggle = signal(true);
   placeholders = {
     title: 'Reactividad con signals',
     body: 'En Angular, utilizamos signals para crear y administrar el estado...',
+    image:
+      'https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4',
   };
+  toggleImageField() {
+    this.imageToggle.set(!this.imageToggle());
+  }
+  toggleDeleteModal() {
+    this.deleteModalToggle.set(!this.deleteModalToggle());
+  }
+  deleteForm() {}
 }
